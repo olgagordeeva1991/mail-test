597e49025bc75d75597ccf3ae2d0b33a9808a4b7
olgagordeeva1991 <gordeeva1991@mail.ru>
2021-02-22 19:51:43 +0300

    selenium grid

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -2,8 +2,14 @@ package test;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
@@ -11,10 +17,19 @@ public class BaseTest {
 
     @BeforeClass
     public static void setUp() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/driver/chromedriver.exe");
 
-        driver = new ChromeDriver();
+        DesiredCapabilities capability = new DesiredCapabilities();
+        capability.setBrowserName("chrome");
 
+//        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/driver/chromedriver.exe");
+//
+//        driver = new ChromeDriver();
+
+        try {
+            driver= new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         driver.get("https://mail.ru/");
     }
 
