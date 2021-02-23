9431c514dfcd282cafce875a260999ff5b74b81d
olgagordeeva1991 <gordeeva1991@mail.ru>
2021-02-23 12:34:48 +0300

    selenium grid with remote and local2

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
